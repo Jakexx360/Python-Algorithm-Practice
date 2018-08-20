1de6f3145ef337e48f33473236cd44098240e3e9
jaketaylor <jake@privategrity.com>
2018-08-20 17:49:01 -0400

    Updated substring to return those characters

--- longestCommonSubstring.py
+++ longestCommonSubstring.py
@@ -1,4 +1,4 @@
-# Determine the length of the longest common substring of two strings in O(m*n) time
+# Determine the length of the longest common substring of two strings and return it in O(m*n) time
 def longest_common_substring(str1, str2):
     # Initialize 2D array of results O(m*n)
     results = [[0 for x in range(len(str2) + 1)] for x in range(len(str1) + 1)]
@@ -18,16 +18,18 @@ def longest_common_substring(str1, str2):
         i += 1
     # Traverse the 2D array to find the largest result O(m*n)
     result = 0
+    x = 0
     i = 1
     while i <= len(str1):  # O(m)
         j = 1
         while j <= len(str2):  # * O(n)
             if result < results[i][j]:
                 result = results[i][j]
+                x = j
             j += 1
         i += 1
-    # Return number of matching characters
-    return result
+    # Return number of matching characters and those characters
+    return result, str2[x - result:x]
 
 print(longest_common_substring("my algorithms class", "algo dog cat test class"))  # 6
 print(longest_common_substring("cat", "dog"))  # 0
