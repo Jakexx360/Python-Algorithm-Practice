73fa2a5a6761adbcbd8e76c171f6b5a3fabbf2b2
jaketaylor <jake@privategrity.com>
2018-08-20 16:51:48 -0400

    Some cleanup

--- formatLicense.py
+++ formatLicense.py
@@ -6,23 +6,26 @@
 # Given S = "r" and K = 1, the function should return "R".
 
 def solution(S, K):
+    # Capitalize
     S = S.upper()
+    # Remove hyphens
     new = ""
     for c in S:
         new += c if c != "-" else ""
+    # Determine size of first group
     first_size = len(new) % K
 
     i = 0
     result = ""
     for c in new:
+        # If first group or appropriate group size, insert hyphen
         if i != 0 and (i == first_size or (i - first_size) % K == 0):
             result += "-"
-            result += c
-        else:
-            result += c
+        # Append the character and increment
+        result += c
         i += 1
     return result
 
 
-print(solution("ggdfgg-hhhhy", 2))
-print(solution("g", 1))
+print(solution("ggdfgg-hhhhy", 6))
+print(solution("gggg", 3))
