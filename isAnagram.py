5ab243a89e69af2ff4d2f59331ba10e345cbfdc9
jaketaylor <jake@privategrity.com>
2018-08-21 12:13:36 -0400

    Added big O to anagram

--- isAnagram.py
+++ isAnagram.py
@@ -1,23 +1,23 @@
-# Checks if one string is an anagram of another
+# Checks if one string is an anagram of another O(n*m)
 def is_anagram(str1, str2):
     # Count letters in str1
     letters1 = dict()
-    for c in str1:
-        letters1[c] = letters1.get(c, 0) + 1
+    for c in str1:  # O(n)
+        letters1[c] = letters1.get(c, 0) + 1  # O(1)
     # Count letters in str2
     letters2 = dict()
-    for c in str2:
-        letters2[c] = letters2.get(c, 0) + 1
+    for c in str2:  # O(m)
+        letters2[c] = letters2.get(c, 0) + 1  # O(1)
     # Compare the letter dict
-    for key, value in letters1.items():
+    for key, value in letters1.items():  # O(n)
         # If the values for this key are the same
-        if letters2.get(key, -1) == value:
+        if letters2.get(key, -1) == value:  # O(1)
             # Delete the key from the second letter dict
-            del (letters2[key])
+            del (letters2[key])  # O(1)
         else:
             return False
     # If second letter dict is empty, all values were equal
-    return len(letters2) == 0
+    return len(letters2) == 0  # O(1)
 
 
 print(is_anagram("test", "ttse"))  # True
