00013f150726a74a242a1346afbf2f6dea41a9ec
jaketaylor <jake@privategrity.com>
2018-08-30 19:54:54 -0400

    Improved remDups

--- remDups.py
+++ remDups.py
@@ -2,26 +2,10 @@
 def remove_duplicates(input_array):
     # Sort the input array
     sorted_array = sorted(input_array)  # O(nlogn)
-    # Create array that will contain unique sorted elements of input_array
-    sorted_output = []
-
-    i = 0
-    while i < len(sorted_array):  # O(n), each element is only traversed once
-        offset = 1
-
-        # Copy the element to the output array
-        sorted_output.append(sorted_array[i])
-
-        # While the next element is equal to this element, add to offset
-        while i + offset < len(sorted_array) and sorted_array[i] == sorted_array[i + offset]:
-            i += 1
-
-        # Add offset to i which skips all the duplicate elements
-        i += offset
 
     # Create hash map / dictionary to add unique values of sorted_output to
     output_map = {}
-    for element in sorted_output:  # O(n)
+    for element in sorted_array:  # O(n)
         output_map.update({element: element})
 
     # Create array that will contain sorted_output with the original array order
